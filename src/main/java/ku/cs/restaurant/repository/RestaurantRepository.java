e317ad9f57872b709fe9a677ad1d3fbcc1e09cb1
Tupp <tupp.k@ku.th>
2023-08-27 22:35:17 +0700

    first commit

---
+++ src/main/java/ku/cs/restaurant/repository/RestaurantRepository.java
@@ -0,0 +1,18 @@
+package ku.cs.restaurant.repository;
+
+
+import ku.cs.restaurant.entity.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
+import java.util.UUID;
+
+
+@Repository
+public interface RestaurantRepository
+        extends JpaRepository<Restaurant, UUID> { //<Generic type, primary key>
+    Restaurant findByName(String name);
+    List<Restaurant> findByLocation(String location);
+}
